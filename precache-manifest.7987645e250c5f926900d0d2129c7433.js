7a03116e86ecf45cb72798ee2a3de60991aa519f
Juan Ruiz Valencia <juan7732@gmail.com>
2019-11-03 23:21:56 -0600

    Updates

---
+++ precache-manifest.7987645e250c5f926900d0d2129c7433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026feeee492578dd62bc739bb4693fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b5ff43e7424b3a430e",
+    "url": "/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "0b8462479227d3140b1b",
+    "url": "/static/css/main.41ef3077.chunk.css"
+  },
+  {
+    "revision": "74b5ff43e7424b3a430e",
+    "url": "/static/js/2.29a4cd1a.chunk.js"
+  },
+  {
+    "revision": "0b8462479227d3140b1b",
+    "url": "/static/js/main.f3f845ee.chunk.js"
+  },
+  {
+    "revision": "7c789216225cc5e11cc2",
+    "url": "/static/js/runtime-main.0ccc2c17.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
